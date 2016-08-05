ad3b7c84d73285c73f4a5b980c8c50637ef65e03
koyo <koyo@koyo.kr>
2016-08-05 23:41:28 +0900

    block!

--- drop.c
+++ drop.c
@@ -1,9 +1,11 @@
 #include "struct.h"
 
-bool filter_true = false;
+bool filter = false;
 
 int fgetline(FILE *fp);
-bool filter(FILE *fp, void *data);
+int fgetlinelen(FILE *fp);
+
+char *gethost(void *data);
 static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg, struct nfq_data *nfa, void *data);
 
 inline void _close(struct nfq_handle *h, struct nfq_q_handle *qh) {
@@ -15,14 +17,37 @@ inline void _close(struct nfq_handle *h, struct nfq_q_handle *qh) {
 
 void *qthread(void *p) {
 	struct thread_arg *arg = (struct thread_arg *)p;
-	puts("\npress enter key to exit~\n");
-	getchar();
-	_close(arg->h, arg->qh);
-	exit(0);
+	puts("[!] press enter key to exit~");
+	while (true) {
+		getchar();
+		_close(arg->h, arg->qh);
+		exit(0);
+	}
 }
 
 void sig_handler(int signo) {
 	exit(1);
+}
+
+int _strlen(const char *str) {
+	int i;
+	for (i = 0; str[i] != '\0'; i++);
+	return i; 
+}
+
+char *_strcpy(char *dst, const char *src) {
+	while (*src != '\0')
+		*dst++ = *src++;
+	*dst = *src;
+	return dst;
+}
+
+int _strcmp(const void *a, const void *b) {
+	while (*(char *)a == *(char *)b && *(char *)a != '\0' && *(char *)b != '\0') {
+		(char *)a++;
+		(char *)b++;
+	}
+	return *(char *)a - *(char *)b;
 }
 
 int main() {
@@ -47,18 +72,51 @@ int main() {
 
 	FILE *fp;
 	fp = fopen("mal_site.txt", "r");
-	char buf2[fgetline(fp)][128];
+
+	int line, linelen;
+	line = fgetline(fp);
+	fseek(fp, 0, SEEK_SET);
+	linelen = fgetlinelen(fp) + 1;
+	fseek(fp, 0, SEEK_SET);
+
+	char site[line][linelen];
+	for (int i = 0; i < line; i++) {
+		char buf_temp[linelen];
+		if (fgets(buf_temp, linelen, fp) == NULL) break;
+		if (buf_temp[_strlen(buf_temp)-2] == '/')
+			buf_temp[_strlen(buf_temp)-2] = '\0';
+		else
+			buf_temp[_strlen(buf_temp)-1] = '\0';
+		_strcpy(site[i], buf_temp + 7);
+	}
+	fclose(fp);
+
+	qsort(site, line, linelen, _strcmp);
+	//bsearch(key, site, line, linelen, _strcmp);
+
+	/* test */
+	/*for (int i = 0; i < line; i++)
+	 *	printf("%s\n", site[i]);
+	 */
 
 	struct thread_arg arg = { h, qh };
 	pthread_t thread;
 	pthread_create(&thread, NULL, qthread, (void *)&arg);
 	pthread_detach(thread);
 
+	char *host;
 	while (true) {
 		rv = recv(fd, buf, sizeof(buf), 0);
 		if (rv >= 0) {
 			/* puts("pkt received"); */
-			filter_true = filter(fp, buf);
+			host = gethost(buf);
+			if (host != NULL)
+				if (bsearch(host, site, line, linelen, _strcmp) != NULL) {
+					printf("\x1b[31m[BLOCK]\x1b[0m %s\n", host);
+					filter = true;
+				}
+				else
+					filter = false;
 			nfq_handle_packet(h, buf, rv);
 			continue;
 		}
@@ -91,38 +149,56 @@ int fgetline(FILE *fp) {
 	return line;	
 }
 
-bool filter(FILE *fp, void *data) {
+int fgetlinelen(FILE *fp) {
+	char buf[4096];
+	char *p;
+	int size;
+	int i, linelen = 0;
+
+	while(!feof(fp)) {
+		size = fread(buf, 1, sizeof(buf), fp);
+		for (i = 0, p = buf; p < buf+size; i++, p++)
+			if (*p == '\n') {
+				if (linelen < i) linelen = i;
+				i = 0;
+			}
+	}
+
+	return linelen;
+}
+
+char *gethost(void *data) {
 	uint8_t     *cp;
 	struct ip   *ip;
 	struct tcp  *tcp;
 	struct http  http;
 
 	ip = (struct ip *)((char *)data + 44);
-	if (ip->ver != IPV4_VERSION) return false;
-	if (ip->pro != PROTOCOL_TCP) return false;
+	if (ip->ver != IPV4_VERSION) return NULL;
+	if (ip->pro != PROTOCOL_TCP) return NULL;
 
 	tcp = (struct tcp *)((char *)ip + ip->len_h * 4);
 	http.get = (uint32_t *)((char *)tcp + tcp->off * 4);
-	if (ntohs(ip->len_t) <= 40) return false;
-	if (*http.get != STRING_GET) return false;
+	if (ntohs(ip->len_t) <= 40) return NULL;
+	if (*http.get != STRING_GET) return NULL;
 
 	cp = (uint8_t *)http.get;
 	while (*cp++ != '\r');
-	if (*cp++ != '\n') return false;
+	if (*cp++ != '\n') return NULL;
 
 	http.host = cp += 6;
 	while (*cp != '\r') cp++;
 	*cp = '\0';
-	printf("%s\n", http.host);
+	//printf("%s\n", http.host);
 
-	return false;
+	return http.host;
 }
 
 static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg, struct nfq_data *nfa, void *data) {
 	struct nfqnl_msg_packet_hdr *ph;
 	ph = nfq_get_msg_packet_hdr(nfa);
 
-	if (filter_true)
+	if (filter)
 		return nfq_set_verdict(qh, ntohl(ph->packet_id), NF_DROP, 0, NULL);
 	else
 		return nfq_set_verdict(qh, ntohl(ph->packet_id), NF_ACCEPT, 0, NULL);
